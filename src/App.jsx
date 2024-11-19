8785ab2dd3f0ef92e3e8bb023d5d6e9db1c6f28f
brittany evans <brittanyevans188@gmail.com>
2024-11-19 19:43:25 +1000

    map over todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react';
 import TodoItem from './components/TodoItem';
 
+
 function App() {
 
   const [todos, setTodos] = useState([
-        "Learn about React",
+        "Learn about joy",
         "Meet a friend for lunch",
         "Build a really cool todo app",
     ]);
     return (
           <div>
               <h1>My todo list</h1>
-              <TodoItem text="Finish plus project." />
-              <TodoItem text="Feed cat." />
-              <TodoItem text="Be awesome." />
+              {todos.map((todo, index) => (
+                <TodoItem text={todo} key={index} />
+           ))}
           </div>
       );
 }
