43daf6cf71f1bbe87dea71038b7dc2f0f6f0ac4a
brittany evans <brittanyevans188@gmail.com>
2024-11-19 20:05:54 +1000

    apply styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import TodoItem from './components/TodoItem';
 import TodoForm from './components/TodoForm';
+import './App.css'
 
 function App() {
 
@@ -16,12 +17,12 @@ function App() {
   };
 
     return (
-          <div>
-              <h1>My todo list</h1>
-              {todos.map((todo, index) => (
-                <TodoItem text={todo} key={index} />
-           ))}
-           <TodoForm addTodo={addTodo} />
+      <div className='app'>
+        <h1 className='todo-list'>My todo list</h1>
+        {todos.map((todo, index) => (
+          <TodoItem text={todo} key={index} />
+          ))}
+          <TodoForm addTodo={addTodo} />
           </div>
       );
 }
